2f6d2860df35f5a8402d79f47b9d3160fc91672c
samzah <szahir@hotmail.com>
2016-04-07 16:51:46 -0400

    stars

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var $body = $('.banner');
+var $star;
+var width = $body.outerWidth();
+var height = $body.outerHeight();
+
+for (i = 0; i < 75; i++) {
+  $star = $('<div>');
+  $star.addClass = ('star');
+  $star.css({
+    top: Math.random() * height,
+    width: Math.random() * width,
+    opacity: Math.random(),
+    transform: 'rotate(' + Math.random() * 360 + 'deg) scale(' + Math.random() + ')'
+  });
+  $body.append($star);
+}
